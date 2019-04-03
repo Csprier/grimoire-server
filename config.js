b03203ddf37c044f451068b158be3bbff2484a58
Cam Prier <CSPrier@gmail.com>
2019-04-03 12:18:14 -0400

    Added db/seed files and utils/seed-database.js

--- config.js
+++ config.js
@@ -1,10 +1,3 @@
-// exports.PORT = process.env.PORT || 8080;
-// exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/grimoire';
-// exports.TEST_MONGODB_URI = process.env.TEST_MONGODB_URI || 'mongodb://localhost/grimoire-test';
-// exports.JWT_SECRET = process.env.JWT_SECRET;
-// exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
-
-
 module.exports = {
 	PORT: process.env.PORT || 8080,
 	CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
