1d32abbc4c17ba3436de5d1eb1c15d16ac14642e
Cam Prier <CSPrier@gmail.com>
2019-07-23 13:10:31 -0400

    Still testing

--- server.js
+++ server.js
@@ -54,18 +54,17 @@ app.use('/api/auth', authRouter);
 
 // ===============================================================================================
 // Catch-all 404
-app.use(function (req, res, next) {
-  console.error('req', req);
-  console.error('res', res);
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function (req, res, next) {
+//   const err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
 // ===============================================================================================
 // Catch-all Error handler
 // Add NODE_ENV check to prevent stacktrace leak
 app.use(function (err, req, res, next) {
+  console.error('ERROR', err);
   res.status(err.status || 500);
   res.json({
     message: err.message,
