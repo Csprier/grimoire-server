b74464ad19ef847c7264f206e0bb2b4e271ff75f
Cam Prier <CSPrier@gmail.com>
2019-07-23 14:19:12 -0400

    testing deployment still

--- server.js
+++ server.js
@@ -54,21 +54,22 @@ app.use('/api/auth', authRouter);
 
 // ===============================================================================================
 // Catch-all 404
-app.use(function (req, res, next) {
-  // const err = new Error('Not Found');
-  // err.status = 404;
-  next(err);
-});
+// app.use(function (req, res, next) {
+//   // const err = new Error('Not Found');
+//   // err.status = 404;
+//   next(err);
+// });
 
 // ===============================================================================================
 // Catch-all Error handler
 // Add NODE_ENV check to prevent stacktrace leak
 app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: app.get('env') === 'development' ? err : {}
-  });
+  console.error('ERROR', err);
+  // res.status(err.status || 500);
+  // res.json({
+  //   message: err.message,
+  //   error: app.get('env') === 'development' ? err : {}
+  // });
 });
 
 // ===============================================================================================
